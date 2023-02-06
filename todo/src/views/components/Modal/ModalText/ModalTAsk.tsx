21e3def0b6d4b5275bd3b58589a8b13910e6e050
Kornull <wolod1987kor@gmail.com>
2023-02-06 18:13:39 +0300

    feat: add style ModalText

--- todo/src/views/components/Modal/ModalText/ModalTAsk.tsx
+++ todo/src/views/components/Modal/ModalText/ModalTAsk.tsx
@@ -3,7 +3,10 @@ import { ModalTypes } from './types';
 import styles from '../Modal.module.scss';
 
 export const ModalTask = ({ text }: ModalTypes) => {
-  return <div className={styles.modal}>
-    <h3>Your task:</h3>
-    {text}</div>;
+  return (
+    <div className={styles.modal}>
+      <h3 className={styles.modalTitle}>Your task:</h3>
+      <div className={styles.modalText}>{text}</div>
+    </div>
+  );
 };
